a6bffa01b403384c22c2966a95fca23c06f59974
a.adamov <wof_4@mail.ru>
2021-06-20 18:48:08 +0300

    add swich from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { HashRouter, Route, Redirect } from 'react-router-dom';
+import {
+  HashRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
 import BuildDetailsContainer from './components/pages/buildDetails/buildDetailsContainer';
 import BuildHistoryPage from './components/pages/buildHistory/buildHistoryPage';
 import s from './app.module.css';
@@ -9,10 +11,12 @@ import Footer from './components/pagesComponents/footer/footer';
 const App = () => (
   <HashRouter>
     <div className={s.wrapper}>
-      <Route path="/details" component={BuildDetailsContainer} />
-      <Route path="/build" component={BuildHistoryPage} />
-      <Route path="/setting" component={SettingPage} />
-      <Route path="/" render={() => <Redirect to="/build" />} />
+      <Switch>
+        <Route path="/details" component={BuildDetailsContainer} />
+        <Route path="/build" component={BuildHistoryPage} />
+        <Route path="/setting" component={SettingPage} />
+        <Route path="/" render={() => <Redirect to="/build" />} />
+      </Switch>
       <Footer />
     </div>
   </HashRouter>
